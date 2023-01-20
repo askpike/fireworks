ddaffa65880b43bf0b0a7aa03b783062496ba4d0
askpike <123150327+askpike@users.noreply.github.com>
2023-01-20 18:38:09 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# 放烟花效果展示
+
+[https://tzs199373.github.io/firework/fire.html](https://tzs199373.github.io/firework/fire.html)
+
---
+++ talk.js
@@ -0,0 +1,38 @@
+$(function() {
+    $('#yes').click(function(event) {
+        modal('我就知道您一定会来的！！！(^_^)', function() {
+            $('.page_one').addClass('hide');
+            $('.page_two').removeClass('hide');
+            // typeWrite();
+            fireworks();
+
+        });
+    });
+    $('#no').click(function(event) {
+        modal('还在等什么呢？烟火大会要开始啦！', function(){
+            fireworks();
+        });
+    });
+});
+
+
+
+function fireworks() {
+    $('.page_one').addClass('hide');
+    initAnimate();
+}
+
+function modal(content, callback) {
+    var tpl = '<div class="container">'+
+        '<div class="mask"></div>'+
+        '<div class="modal">'+
+        '<p>'+ content +'</p>'+
+        '<button type="button" id="confirm" class="confirm">确定</button>'+
+        '</div>'+
+        '</div>';
+    $('body').append(tpl);
+    $(document).on('click', '.confirm', function() {
+        $('.container').remove();
+        callback();
+    });
+}
